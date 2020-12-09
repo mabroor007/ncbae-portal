95111b686390173943998dac881b9ed29b84c658
mabroordev <mabroordev@mabroordev.dev>
2020-12-08 16:41:07 -1000

    reversed a bit

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,24 +2,13 @@
   <div>
     <form>
       <input id="search" placeholder="Search" type="search" />
-      <div class="selbox">
-        <select id="sel">
-          <option value="name">By Name</option>
-          <option value="roll">By Roll</option>
-          <option value="course">By Course</option>
-        </select>
-      </div>
     </form>
   </div>
 </template>
 
 <script>
-// import { reactive } from "@vue/composition-api";
-
 export default {
   setup() {
-    // const state = reactive({});
-
     return {};
   },
 };
@@ -38,20 +27,5 @@ input#search {
   padding: 12px 16px;
   border-radius: 10px;
   filter: drop-shadow(0 0 37px rgba(0, 0, 0, 0.253));
-}
-.selbox {
-  background-color: #56cade;
-}
-#sel {
-  border: none;
-  background: none;
-  border-radius: 10px;
-  outline: none;
-  font-family: "Poppins";
-  appearance: none;
-}
-#sel option {
-  border: none;
-  outline: none;
 }
 </style>
